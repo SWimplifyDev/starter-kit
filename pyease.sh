ba043fcf7814cb35e76f5b4941d07c11f69d8935
SWimplifyDev <144074256+SWimplifyDev@users.noreply.github.com>
2024-12-05 13:24:52 +1030

    Messagenger

--- pyease.sh
+++ pyease.sh
@@ -8,6 +8,48 @@
 # machine, if so you have the choice whether to install them or not.                             #
 ##################################################################################################
 
+################## Message Printer #######################
+# Define the "MessagePrinter" class using functions
+MessagePrinter(){
+    # Function to initialize the class (like a constructor)
+    int(){
+    INFO_COLOR="\033[1;36m"  # Cyan
+    SUCCESS_COLOR="\033[1;32m" # Green
+    WARNING_COLOR="\033[1;33m" # Yellow
+    ERROR_COLOR="\033[1;31m"   # Red
+    RESET_COLOR="\033[0m"    # Reset    
+    }
+# Function to print info messages
+  info() {
+    local message=$1
+    echo -e "${INFO_COLOR}[Info]${RESET_COLOR} ${message}"
+  }
+
+  # Function to print success messages
+  success() {
+    local message=$1
+    echo -e "${SUCCESS_COLOR}[Success]${RESET_COLOR} ${message}"
+  }
+
+  # Function to print warning messages
+  warning() {
+    local message=$1
+    echo -e "${WARNING_COLOR}[Warning]${RESET_COLOR} ${message}"
+  }
+
+  # Function to print error messages
+  error() {
+    local message=$1
+    echo -e "${ERROR_COLOR}[Error]${RESET_COLOR} ${message}"
+  }
+
+}
+
+MessagePrinter
+init
+
+########################## End Message Printer Code ##########################
+
 # Folder names to be created
 VENV_DIR=".venv"
 VSCODE_DIR=".vscode"
@@ -38,11 +80,14 @@ set_venv(){
 
 
 delete_venv(){
-    echo -e "\033[1;34m[Info]\033[0m \033[34m Deleting .venv \033[0m"
+    info "Deleting virtual environment..."
     rm -r "$VENV_DIR"
     rm -r "$VSCODE_DIR"
-    echo -e "\033[1;32m[Info]\033[0m \033[32m Deleted \033[0m"
+    success "Deleted."
 }
+
+
+
 
 
 # Funcion to initialize a python project
